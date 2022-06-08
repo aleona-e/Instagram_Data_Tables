8945f85f5c218d6dfa7c82c0c0da36a280942fe0
aleona-e <ale.elsin92@gmail.com>
2022-06-08 19:47:09 +0000

    implementing startWars tables

--- src/models.py
+++ src/models.py
@@ -7,24 +7,60 @@ from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__= 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    firstName = Column(String(250), nullable=False)
+    lastName = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favorites(Base):
+    __tablename__= 'favorites'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    userId = Column(Integer, ForeignKey('user.id'), nullable=False)
+    characterId = Column(Integer, ForeignKey('character.id'), nullable=True)
+    planetId = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    vehicleId = Column(Integer, ForeignKey('vehicle.id'), nullable=True)
+
+class Planets(Base):
+    __tablename__='planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String) 
+    terrain = Column(String)
+    population = Column(Integer)
+    diameter = Column(Integer) 
+    surfaceWater = Column(Integer) 
+    gravity = Column(String)
+    climate = Column(String) 
+    orbitalPeriod = Column(Integer)
+    rotationPeriod = Column(Integer)
+
+class Characters(Base):
+    __tablename__='character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    gender = Column(String)
+    hairColor = Column(String)
+    eyeColor = Column(String)
+    mass = Column(Integer)
+    height = Column(Integer)
+    birthYear = Column(String) 
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+
+class Vehicles(Base):
+    __tablename__='vehicle'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    model = Column(String)
+    vehicleClass = Column(String)
+    manufacturers = Column(String)
+    cost = Column(Integer)
+    length = Column(Integer)
+    crew = Column(Integer)
+    passengers = Column(Integer)
+    pilots_id = Column(Integer, ForeignKey('character.id'))
+
 
     def to_dict(self):
         return {}
